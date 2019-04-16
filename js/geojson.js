2b12241ab3311ada17dfbdca0c569d9f0a1253a5
Nishigandha Sontakke <sontakke@wisc.edu>
2019-04-16 18:49:03 -0500

    Contour Layer added, weather widget added
    Contour Layer added, weather widget added

--- js/geojson.js
+++ js/geojson.js
@@ -7,12 +7,65 @@ var searchLayer;
 function createMap(){
     //create the map
     mapboxgl.accessToken = 'pk.eyJ1IjoibmlzaGlkaWxpcHNvbnRha2tlIiwiYSI6ImNqY3FucHJ4azAzNXgzM3MwbGRvM3M2YWsifQ.Mwh9X4xZhkSBBCTfBlZHEQ';
-    const map = new mapboxgl.Map({
+    var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/nishidilipsontakke/cjuhnd62s559x1fqmt6kccmfe',
       center: [86.924374,27.988195],
       zoom: 10.0
     });
+
+    map.on('load', function () {
+    
+         
+        map.addSource('contours', {
+        type: 'vector',
+        url: 'mapbox://mapbox.mapbox-terrain-v2'
+        });
+        map.addLayer({
+        'id': 'contours',
+        'type': 'line',
+        'source': 'contours',
+        'source-layer': 'contour',
+        'layout': {
+        'visibility': 'visible',
+        'line-join': 'round',
+        'line-cap': 'round'
+        },
+        'paint': {
+        'line-color': '#877b59',
+        'line-width': 1
+        }
+        });
+        });
+        var toggleableLayerIds = [ 'contours'];
+ 
+        for (var i = 0; i < toggleableLayerIds.length; i++) {
+        var id = toggleableLayerIds[i];
+         
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = id;
+         
+        link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+         
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+         
+        if (visibility === 'visible') {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        this.className = '';
+        } else {
+        this.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+        };
+         
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+        }
 };
 
 
@@ -33,5 +86,9 @@ $('#panel').append('<div class="row" style="text-align: center;"><div class="col
 
    
 };
+
+$("#buttonweather").click(function(){
+    $("#weatherwidget").toggle();
+  });
 
 $(document).ready(createMap); // calling create map function on document ready
