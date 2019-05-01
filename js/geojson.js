e62081934d62a96407b69071eb929729c613772b
aaronj1 <46979125+aaronj1@users.noreply.github.com>
2019-05-01 16:59:45 -0500

    Minor centering adjustment

--- js/geojson.js
+++ js/geojson.js
@@ -15,7 +15,7 @@ function createMap(){
     var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/nishidilipsontakke/cjuhnd62s559x1fqmt6kccmfe',
-      center: [86.9, 27.988],
+      center: [86.901, 27.988],
       zoom: 13,
       maxBounds: bounds
     });
